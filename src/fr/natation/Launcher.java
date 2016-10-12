170b758fee5a0bf1c837bb00f8746e0f8beb8ef1
ginguene <ginguene@PC-de-ginguene>
2016-10-12 06:48:08 +0100

    Gestion des backup

--- src/fr/natation/Launcher.java
+++ src/fr/natation/Launcher.java
@@ -1,8 +1,15 @@
 package fr.natation;
 
-import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
 
-import fr.natation.view.NatationFrame;
+import org.apache.log4j.Logger;
 
 public class Launcher {
 
@@ -18,7 +25,45 @@ public class Launcher {
             LOGGER.error("Failed to load Look and Feel.", e);
         }
 
-        new NatationFrame().setVisible(true);
+        backup();
+
+        // new NatationFrame().setVisible(true);
+
+    }
+
+    public static void backup() throws IOException {
+        File backupDir = new File("backup/");
+        if (!backupDir.exists()) {
+            backupDir.mkdirs();
+        }
+
+        String backupFilneName = "backup/" + new SimpleDateFormat("yyyy-MM-dd-ss").format(new Date()) + "_" + "natation.db";
+        if (!new File(backupFilneName).exists()) {
+            Files.copy(Paths.get("natation.db"), Paths.get(backupFilneName));
+            new File(backupFilneName).setLastModified(new Date().getTime());
+            LOGGER.debug("Create backup " + backupFilneName);
+        }
+
+        // clean old backup
+        File[] files = backupDir.listFiles();
+
+        Arrays.sort(files, new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
+            }
+        });
+
+        int maxBackup = 10;
+        int currentBackup = 0;
+        for (File f : files) {
+            System.out.println(f.getName());
+            if (currentBackup > maxBackup) {
+                LOGGER.debug("Delete backup " + backupFilneName);
+                f.delete();
+            }
+            currentBackup++;
+        }
 
     }
 }
